6e47168c3cbc95d9b5a17d207ad5c82c1a4aacab
Priscy Antonio Reales Mozo <realespriscy@hotmail.com>
2024-07-12 21:44:18 -0500

    first commit

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { envs } from './common';
+import { Logger } from '@nestjs/common';
+
+async function bootstrap() {
+  const logger = new Logger('Main');
+
+  const app = await NestFactory.create(AppModule);
+  await app.listen(envs.port);
+
+  logger.log(`Products Microservice running on port ${envs.port}`);
+
+}
+bootstrap();
